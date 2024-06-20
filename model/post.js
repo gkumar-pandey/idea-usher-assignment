bfec1af0065abaa3e33bc9768589e7434cc040af
gkumar-pandey <gautamshekhar078@gmail.com>
2024-06-21 00:38:54 +0530

    post model created

---
+++ model/post.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    tags: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Tag",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", postSchema);
+module.exports = Post;
