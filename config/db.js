53e71cc94bc2490b9ba8b911972474b644cf4b2e
gkumar-pandey <gautamshekhar078@gmail.com>
2024-06-21 15:23:26 +0530

    fix:db connection url

--- config/db.js
+++ config/db.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
 const connectDb = async () => {
-  const dbURL = process.env.DB_URL;
+  const DB_USER = process.env.DB_USER;
+  const DB_PASSWORD = process.env.DB_PASSWORD;
+  const dbURL = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@neog.a4r4b3k.mongodb.net/?retryWrites=true&w=majority&appName=Neog`;
   try {
     const connect = await mongoose.connect(dbURL);
     if (connect) {
